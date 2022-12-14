bc253cec734fbe5b1513e4b6e302e09700b687ff
abdel-rahman elshafei <114206498+abdelshafei@users.noreply.github.com>
2022-12-14 13:46:20 -0500

    Update Control statements.py

---
+++ Control statements.py
@@ -0,0 +1,17 @@
+condition =  True
+
+if condition == True:
+    print("i am here!")
+else:
+    print("i am not here.")
+
+# the output for the above conditional statement will be "i am here!" since the condition variable is already True
+
+condition = False    
+
+if condition == True:
+    print("i am here!")
+else:
+    print("i am not here.")
+
+# the output for the above conditional statement will be "i am not here." since the condition variable is already False  
